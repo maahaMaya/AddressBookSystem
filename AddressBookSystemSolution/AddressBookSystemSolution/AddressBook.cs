b3d53a455b832fe985b3a35d1561ba9313822277
SOURAV <devbhsourav@gmail.com>
2022-12-15 19:01:25 +0530

    [sourav]add : AddressBook-CreatedAndAddContact

---
+++ AddressBookSystemSolution/AddressBookSystemSolution/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemSolution
+{
+    internal class AddressBook
+    {
+        ContackBook contackBook = new ContackBook();
+        public void AddcontackBookIncontackBookBook()
+        {
+            Console.Write("Enter  FirstName: ");
+            contackBook.firstName = Console.ReadLine();
+            Console.Write("Enter  LastName: ");
+            contackBook.lastName = Console.ReadLine();
+            Console.Write("Enter  Address: ");
+            contackBook.address = Console.ReadLine();
+            Console.Write("Enter  City: ");
+            contackBook.city = Console.ReadLine();
+            Console.Write("Enter  State: ");
+            contackBook.state = Console.ReadLine();
+            Console.Write("Enter  Zip: ");
+            contackBook.zipCode = Console.ReadLine();
+            Console.Write("Enter  PhoneNumber: ");
+            contackBook.phoneNumber = Console.ReadLine();
+            Console.Write("Enter  Email: ");
+            contackBook.email = Console.ReadLine();
+        }
+        public void DisplayContactBook()
+        {
+            Console.WriteLine("Contact Derails : \n" + "First Name : " + contackBook.firstName + "\n" + "Last Name : " + contackBook.lastName
+                                    + "\n" + "Address is : " + contackBook.address + "\n" + "City is : " + contackBook.city + "\n" + "State is : " + contackBook.state
+                                    + "\n" + "Zip is : " + contackBook.zipCode + "\n" + "Phone Number is :" + contackBook.phoneNumber + "\n" + "Email is : " + contackBook.email);
+        }
+    }
+}
--- AddressBookSystemSolution/AddressBookSystemSolution/Program.cs
+++ AddressBookSystemSolution/AddressBookSystemSolution/Program.cs
@@ -5,7 +5,25 @@ namespace AddressBookSystemSolution
     {
         static void Main(string[] args)
         {
-
+            AddressBook addressBook = new AddressBook();
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Enter the option : 1.AddContact 2.DisplayContact 3.Exit: ");
+                int option = Convert.ToInt16(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        addressBook.AddcontackBookIncontackBookBook();
+                        break;
+                    case 2:
+                        addressBook.DisplayContactBook();
+                        break;
+                    default:
+                        flag= false;
+                        break;
+                }
+            }
         }
     }
 }
